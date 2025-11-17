e6f009a0661fcb5446f7e168e040e6cbb5cf2262
NestorAndres1215 <atiroandres@gmail.com>
2025-11-16 19:16:14 -0500

    subiendo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { PrincipalComponent } from './features/home/principal/principal.component';
+import { Oauth2RedirectComponent } from './features/auth/oauth2-redirect/oauth2-redirect.component';
+import { ErrorComponent } from './shared/components/error/error.component';
 
 export const routes: Routes = [
 
       { path: '', component: PrincipalComponent, pathMatch: 'full' },
+      { path: 'oauth2/redirect', component: Oauth2RedirectComponent },
+      { path: 'error', component: ErrorComponent },
 ];
---
+++ frontend/src/app/features/auth/oauth2-redirect/oauth2-redirect.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oauth2-redirect.component',
+  imports: [],
+  templateUrl: './oauth2-redirect.component.html',
+  styleUrl: './oauth2-redirect.component.css',
+})
+export class Oauth2RedirectComponent {
+
+}
