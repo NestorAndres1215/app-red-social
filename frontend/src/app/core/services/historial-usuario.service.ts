5beb014abc27a85039dab959398aeaa2615d4438
NestorAndres1215 <atiroandres@gmail.com>
2025-11-19 13:43:09 -0500

    service de historial usairo

--- frontend/src/app/core/services/historial-usuario.service.ts
+++ frontend/src/app/core/services/historial-usuario.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HistorialUsuarioService {
-  
+
+  private backendUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<any> {
+    return this.http.get<any>(`${this.backendUrl}/historial-usuario/listar`);
+  }
+
+  listarCodigo(codigo: string): Observable<any> {
+    return this.http.get<any>(`${this.backendUrl}/historial-usuario/listar/codigo/${codigo}`);
+  }
+
+  listarHistorial(username: string, estado: string): Observable<any> {
+    return this.http.get<any>(`${this.backendUrl}/historial-usuario/listar/historial/${username}/${estado}`);
+  }
+
+
+  inactivar(codigo: string): Observable<any> {
+    return this.http.delete<any>(`${this.backendUrl}/historial-usuario/inactivar/estado/${codigo}`);
+  }
 }
