f7a24294d2fed4452ec6c85a6a756e77e5affb03
NestorAndres1215 <atiroandres@gmail.com>
2025-11-21 18:49:15 -0500

    subiendo

--- frontend/src/app/core/layout/sidebar-user/sidebar-user.component.ts
+++ frontend/src/app/core/layout/sidebar-user/sidebar-user.component.ts
@@ -1,17 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { LayoutUserComponent } from "../layout-user/layout-user.component";
+import { RouterModule, RouterOutlet } from "@angular/router";
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar-user',
   templateUrl: './sidebar-user.component.html',
   styleUrls: ['./sidebar-user.component.css'],
-  imports: [LayoutUserComponent]
+  imports: [LayoutUserComponent,  RouterModule, CommonModule]
 })
 export class SidebarUserComponent implements OnInit {
 
-  constructor() { }
+  isNavActive: boolean = false;
+  isLoggedIn: boolean = true;
+  usuario: any = null;
+  displayName: string = '';
+  nombre: String = '';
 
-  ngOnInit() {
+  constructor(private authService: AuthService) { } // nombre consistente
+
+  ngOnInit(): void {
+    this.loadUsername();
+  }
+
+  toggleNav(): void {
+    this.isNavActive = !this.isNavActive;
+  }
+
+  logout(): void {
+    // Aquí implementa tu lógica de cerrar sesión
+    console.log('Usuario ha cerrado sesión');
+    this.isLoggedIn = false;
+    this.usuario = null;
+    this.displayName = '';
+  }
+
+  loadUsername(): void {
+    this.authService.getCurrentUser().subscribe({
+      next: (user) => {
+        this.usuario = user;
+  
+        // Si username está vacío, usar email
+        this.displayName = this.usuario.username?.trim() || this.usuario.email;
+        console.log(this.displayName)
+
+      },
+      error: (err) => {
+        console.error('Error al obtener usuario', err);
+      }
+    });
   }
 
 }
