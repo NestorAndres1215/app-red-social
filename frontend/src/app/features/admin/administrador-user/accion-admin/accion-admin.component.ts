6d0bdcc401304ce9bd6043d0766517f446c9b932
NestorAndres1215 <atiroandres@gmail.com>
2025-11-30 01:23:16 -0500

    subiendo

--- frontend/src/app/features/admin/administrador-user/accion-admin/accion-admin.component.ts
+++ frontend/src/app/features/admin/administrador-user/accion-admin/accion-admin.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { TittleComponent } from "../../../../shared/components/tittle/tittle.component";
 
 @Component({
   selector: 'app-accion-admin',
-  imports: [],
+  imports: [TittleComponent],
   templateUrl: './accion-admin.component.html',
   styleUrl: './accion-admin.component.css',
 })
 export class AccionAdminComponent {
+  titulo = 'Acciones del Administrador';
+  icono = 'fas fa-user-gear';
 
 }
